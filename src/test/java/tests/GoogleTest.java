8267eb831c42cb6e14b49b687b83076efb1e7926
Ilya_Malykh <ferras2007@yandex.ru>
2020-12-02 19:27:46 +0300

    Commit

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,2 +1,21 @@
-package tests;public class GoogleTest {
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTest {
+
+    @Test
+    void GoogleSearchTest() {
+        open("https://google.com");
+
+        $(byName("q")).val("selenide").submit();
+
+        $("body").shouldHave(Condition.text("Selenide: лаконичные и стабильные UI тесты на Java"));
+    }
 }
